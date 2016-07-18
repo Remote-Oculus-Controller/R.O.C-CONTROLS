70e31fa45f57b2f180c9f50df12cbd9d4b7b5460
Sylvain Prost <prostg.sylvain@gmail.com>
2016-07-18 16:56:53 +0200

    logged coordinatess

--- robots/gps.go
+++ robots/gps.go
@@ -49,6 +49,7 @@ func NewGPS() *Gps {
 			Long: tpv.Lon + gps.yoff,
 			Ori:  gps.orioff,
 		}
+		log.Printf("Coordinates => %+v\n", m)
 		p := &roc.Packet{
 			ID:     GPS_TAG,
 			Header: H_DCV,
@@ -102,13 +103,8 @@ func (gps *Gps) sim(params map[string]interface{}) interface{} {
 	fmt.Printf("Changing position\n")
 	n := params["mv"].(roc.Mouv)
 
-	fmt.Printf("mouv %+v\n", n)
-	/*	fmt.Printf("offset : x %v y %v\n", 0.000001*math.Cos(n.Angle), 0.000001*math.Sin(-n.Angle))
-		fmt.Printf("Calculated ratio	x => %v ; y => %v", math.Cos(n.Angle), math.Sin(-n.Angle))*/
-	fmt.Printf("Angle deg : %v", n.Angle*180/math.Pi)
 	gps.yoff += 0.000001 * math.Sin(n.Angle)
 	gps.xoff += 0.000001 * math.Cos(n.Angle)
 	gps.orioff = n.Angle * 180 / math.Pi
-	fmt.Printf("GPS %+v", gps)
 	return nil
 }
