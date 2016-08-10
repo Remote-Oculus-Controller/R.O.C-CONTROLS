c68ed5648e87c6ccfb3577cc00defecbe7f05cd2
Neugdae <prostg.sylvain@gmail.com>
2016-08-11 00:29:15 +0200

    first attempt at websocket integration

--- linker.go
+++ linker.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"net"
 
+	"net/http"
+
 	"github.com/Happykat/R.O.C-CONTROLS/misc"
 	"github.com/Happykat/R.O.C-CONTROLS/rocproto"
 	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/websocket"
 )
 
 type Linker struct {
@@ -16,8 +19,10 @@ type Linker struct {
 	lT, rT        bool
 }
 
+//TODO clear TCP
 type Link struct {
 	conn    *net.TCPConn
+	ws      websocket.Conn
 	out, in chan *rocproto.Packet
 }
 
@@ -32,21 +37,21 @@ func NewLinker(lS, rS string, lT, rT bool) *Linker {
 func (l *Linker) Start() {
 	if l.lIp != "" {
 		log.Print("Staring local work")
-		go l.local.startConn(l.lIp, l.lT, &l.remote, rocproto.Packet_CONTROL_SERVER|rocproto.Packet_VIDEO_SERVER)
+		go l.local.startConnTCP(l.lIp, l.lT, &l.remote, rocproto.Packet_CONTROL_SERVER|rocproto.Packet_VIDEO_SERVER)
 	}
 	log.Println("Starting remote work")
-	go l.remote.startConn(l.rIp, l.rT, &l.local, rocproto.Packet_CONTROL_SERVER|rocproto.Packet_VIDEO_CLIENT)
+	go l.remote.startConnWS(l.rIp, l.rT, &l.local, rocproto.Packet_CONTROL_SERVER|rocproto.Packet_VIDEO_CLIENT)
 }
 
 //TODO timeout connection and try
-func (l *Link) startConn(s string, m bool, o *Link, t rocproto.Packet_Section) {
+func (l *Link) startConnTCP(s string, m bool, o *Link, t rocproto.Packet_Section) {
 
 	defer close(l.in)
 	defer close(l.out)
 
 	var listener *net.TCPListener
 
-	log.Print("Starting connection on ", s)
+	log.Println("Starting connection on ", s)
 	tcpAddr, err := net.ResolveTCPAddr("tcp", s)
 	misc.CheckError(err, "resolving address in linker.go/startConn", true)
 	for {
@@ -72,6 +77,153 @@ func (l *Link) startConn(s string, m bool, o *Link, t rocproto.Packet_Section) {
 	}
 }
 
+func (l *Link) startConnWS(s string, m bool, o *Link, t rocproto.Packet_Section) {
+
+	defer close(l.in)
+	defer close(l.out)
+
+	log.Printf("Starting websocket on %v/controls", s)
+	if m {
+		http.HandleFunc("/controls", l.listenWS)
+	}
+}
+
+func checkBuffer(r int, buff []byte) (m rocproto.Packet, err error) {
+
+	if r > -1 {
+		err = proto.Unmarshal(buff[0:r], m)
+	} else {
+		err = proto.Unmarshal(buff[0:], m)
+	}
+	if err != nil {
+		log.Println("Cannot Unmarshall packet", err.Error())
+		log.Println(r, buff[0:r])
+		return nil, err
+	}
+	if m.Magic != MAGIC {
+		log.Println("Wrong packet")
+		return nil, errors.New("Wrong packet")
+	}
+	return m, nil
+}
+
+func routPacket(m rocproto.Packet, l, o *Link, t rocproto.Packet_Type) {
+	if m.Header&uint32(t) != 0 {
+		l.in <- m
+	}
+	if (m.Header&uint32(rocproto.Packet_MASK_DEST))&^uint32(t) != 0 && o.conn != nil {
+		o.out <- m
+	}
+}
+
+//TODO Insert buffer len and check
+func (l *Link) handleConn(o *Link, t rocproto.Packet_Section) {
+
+	buff := make([]byte, 128)
+	quit := make(chan bool)
+	go func() {
+
+		defer func() { quit <- true }()
+
+		m := new(rocproto.Packet)
+		for {
+			r, err := l.conn.Read(buff[0:])
+			if misc.CheckError(err, "Receiving data from conn", false) != nil {
+				return
+			}
+			if m, err = checkBuffer(r, buff); err != nil {
+				continue
+			}
+			routPacket(m, l, o, t)
+		}
+	}()
+	for {
+		select {
+		case <-quit:
+			return
+		case m := <-l.out:
+			b, err := proto.Marshal(m)
+			if misc.CheckError(err, "linker.go/handleConn", false) != nil {
+				continue
+			}
+			_, err = l.conn.Write(b)
+			if misc.CheckError(err, "linker.go/handleConn", false) != nil {
+				return
+			}
+		}
+	}
+}
+
+func (l *Link) listenWS(w http.ResponseWriter, r *http.Request) {
+
+	if l.conn != nil {
+		er := "Connection already taken !!"
+		log.Println(er)
+		w.Write([]byte(er))
+		return
+	}
+	upgrader := websocket.Upgrader{}
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		er := "Error upgrading to websocket"
+		log.Println(er)
+		w.Write([]byte(er))
+		return
+	}
+	log.Println("Client connected", r.RemoteAddr)
+	l.ws = c
+	return
+}
+
+func (l *Link) handleWS(o *Link, t rocproto.Packet_Section) {
+
+	quit := make(chan bool)
+	go func() {
+
+		defer func() { quit <- true }()
+
+		m := new(rocproto.Packet)
+		for {
+			_, buff, err := l.ws.ReadMessage()
+			if misc.CheckError(err, "Receiving data from conn", false) != nil {
+				return
+			}
+			if m, err = checkBuffer(-1, buff); err != nil {
+				continue
+			}
+			routPacket(m, l, o, t)
+		}
+	}()
+	for {
+		select {
+		case <-quit:
+			return
+		case m := <-l.out:
+			b, err := proto.Marshal(m)
+			if misc.CheckError(err, "linker.go/handleWS", false) != nil {
+				continue
+			}
+			err = l.ws.WriteMessage(websocket.BinaryMessage, b)
+			if misc.CheckError(err, "linker.go/handleWS", false) != nil {
+				return
+			}
+		}
+	}
+}
+
+func (l *Linker) Stop() {
+	if l.remote.conn != nil {
+		l.remote.conn.Close()
+		close(l.remote.in)
+		close(l.remote.out)
+	}
+	if l.local.conn != nil {
+		l.local.conn.Close()
+		close(l.local.in)
+		close(l.local.out)
+	}
+}
+
 func (l *Linker) Send(p *rocproto.Packet) error {
 
 	if (p.Header&uint32(rocproto.Packet_VIDEO_CLIENT)) != 0 && l.remote.conn != nil {
@@ -93,67 +245,4 @@ func (l *Linker) RegisterChannel(r bool) chan *rocproto.Packet {
 		return l.remote.in
 	}
 	return l.local.in
-}
-
-func (l *Link) handleConn(o *Link, t rocproto.Packet_Section) {
-
-	buff := make([]byte, 128)
-	quit := make(chan bool)
-	go func() {
-
-		defer func() { quit <- true }()
-
-		m := new(rocproto.Packet)
-		for {
-			r, err := l.conn.Read(buff[0:])
-			if misc.CheckError(err, "Receiving data from conn", false) != nil {
-				return
-			}
-			err = proto.Unmarshal(buff[0:r], m)
-			if err != nil {
-				log.Println("Cannot Unmarshall packet", err.Error())
-				log.Println(r, buff[0:r])
-				continue
-			}
-			log.Println("Reveived packet : %+v", m)
-			if m.Magic != MAGIC {
-				log.Println("Wrong packet")
-				continue
-			}
-			if m.Header&uint32(t) != 0 {
-				l.in <- m
-			}
-			if (m.Header&uint32(rocproto.Packet_MASK_DEST))&^uint32(t) != 0 && o.conn != nil {
-				o.out <- m
-			}
-		}
-	}()
-	for {
-		select {
-		case <-quit:
-			return
-		case m := <-l.out:
-			b, err := proto.Marshal(m)
-			if misc.CheckError(err, "linker.go/handleConn", false) != nil {
-				continue
-			}
-			_, err = l.conn.Write(b)
-			if misc.CheckError(err, "linker.go/handleConn", false) != nil {
-				return
-			}
-		}
-	}
-}
-
-func (l *Linker) Stop() {
-	if l.remote.conn != nil {
-		l.remote.conn.Close()
-		close(l.remote.in)
-		close(l.remote.out)
-	}
-	if l.local.conn != nil {
-		l.local.conn.Close()
-		close(l.local.in)
-		close(l.local.out)
-	}
 }
